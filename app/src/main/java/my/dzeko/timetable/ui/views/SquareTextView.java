a6626fb31c04adec4a4669a097880fe67d85add4
Евгений <dzekomerden@gmail.com>
2018-08-29 14:59:58 +0300

    Change CalendarTextView name to SquareTextView.

--- app/src/main/java/my/dzeko/timetable/ui/views/SquareTextView.java
+++ app/src/main/java/my/dzeko/timetable/ui/views/SquareTextView.java
@@ -5,16 +5,16 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 
-public class CalendarDayTextView extends AppCompatTextView {
-    public CalendarDayTextView(Context context) {
+public class SquareTextView extends AppCompatTextView {
+    public SquareTextView(Context context) {
         super(context);
     }
 
-    public CalendarDayTextView(Context context, @Nullable AttributeSet attrs) {
+    public SquareTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public CalendarDayTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public SquareTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
